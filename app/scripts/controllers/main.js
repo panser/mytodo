f82cea9d2627e8a7e0b90608129b62b92455c615
panser <gostroy@gmail.com>
2016-02-06 08:31:55 +0200

    step10: use angular-local-storage http://yeoman.io/codelab/local-storage.html

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,36 +1,23 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope, localStorageService) {
 
-  // load the controller's module
-  beforeEach(module('mytodoApp'));
+    var todosInStore = localStorageService.get('todos');
 
-  var MainCtrl,
-    scope;
+    $scope.todos = todosInStore || [];
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
-  }));
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
 
-  it('should have no items to start', function () {
-    expect(scope.todos.length).toBe(0);
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+
   });
-
-  it('should add items to the list', function () {
-    scope.todo = 'Test 1';
-    scope.addTodo();
-    expect(scope.todos.length).toBe(1);
-  });
-
-  it('should add then remove an item from the list', function () {
-    scope.todo = 'Test 1';
-    scope.addTodo();
-    scope.removeTodo(0);
-    expect(scope.todos.length).toBe(0);
-  });
-
-});
