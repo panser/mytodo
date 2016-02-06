5a94ecfcbbb0e6ba7fe7ed9e707b9f9c402ccf88
panser <gostroy@gmail.com>
2016-02-06 08:16:14 +0200

    step8: update tests http://yeoman.io/codelab/write-unit-tests.html

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,36 @@
 'use strict';
 
-angular.module('mytodoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
-    $scope.addTodo = function () {
-      $scope.todos.push($scope.todo);
-      $scope.todo = '';
-    };
-    $scope.removeTodo = function (index) {
-      $scope.todos.splice(index, 1);
-    };
+describe('Controller: MainCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('mytodoApp'));
+
+  var MainCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    MainCtrl = $controller('MainCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should have no items to start', function () {
+    expect(scope.todos.length).toBe(0);
   });
+
+  it('should add items to the list', function () {
+    scope.todo = 'Test 1';
+    scope.addTodo();
+    expect(scope.todos.length).toBe(1);
+  });
+
+  it('should add then remove an item from the list', function () {
+    scope.todo = 'Test 1';
+    scope.addTodo();
+    scope.removeTodo(0);
+    expect(scope.todos.length).toBe(0);
+  });
+
+});
